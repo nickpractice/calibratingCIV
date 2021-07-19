d78dcad08627719464cc0ffd24c36b04e38103d4
nickpractice <nickbbigalke@gmail.com>
2021-07-19 12:39:20 -0700

    Calib 5.0

--- startTracking.js
+++ startTracking.js
@@ -11,7 +11,7 @@ AFRAME.registerComponent('start-tracking', {
       this.el.sceneEl.appendChild(CIV) 
       CIV.setAttribute('position', '0 0 -5')//Change Position
       CIV.setAttribute('rotation', '0 225 0')
-      CIV.setAttribute('scale', '1 1 1')//Change Scale
+      CIV.setAttribute('scale', '2 2 2')//Change Scale
       CIV.setAttribute('visible', 'true')
       text.setAttribute('visible', 'false')
       container.style.display = "none"
